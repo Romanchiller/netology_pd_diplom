c975827b565ae7997cd24e19c02726f968a4ec61
Romanchiller <VacuuM2007@yandex.ru>
2024-08-02 23:16:54 +0500

    Docker_Compose_Ready

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Запуск приложения #
 
 - docker-compose up -d
-- docker-compose run django python manage.py migrate
 - docker-compose run django python manage.py createsuperuser
 
+
+# Запуск Celery если приложение запущено без докера #
 - celery -A netology_pd_diplom.celery worker --loglevel=info
--- netology_pd_diplom/settings.py
+++ netology_pd_diplom/settings.py
@@ -162,11 +162,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# CELERY_BROKER_URL = os.getenv("CELERY_BROKER", "redis://redis:6379/0")
-# CELERY_RESULT_BACKEND = os.getenv("CELERY_BACKEND", "redis://redis:6379/0")
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_BACKEND", "redis://redis:6379/0")
 
 
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
-# CELERY_TASK_SERIALIZER = "json"
-# CELERY_RESULT_SERIALIZER = "json"
+# CELERY_BROKER_URL = "redis://localhost:6379/0"
+# CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
